05d936a594cf22d56a306bfc78b445060e85a929
root <root@homeserver>
2015-08-05 11:56:29 +0200

    Fix generated secret length

--- lib/deploy_it/utils.rb
+++ lib/deploy_it/utils.rb
@@ -96,9 +96,9 @@ module DeployIt
 
       def generate_secret(length)
         length = length.to_i
-        secret = SecureRandom.base64(length)
+        secret = SecureRandom.base64(length * 2)
         secret = secret.gsub(/[\=\_\-\+\/]/, '')
-        secret = secret.split(//).sample(length - 1).join('')
+        secret = secret.split(//).sample(length).join('')
         secret
       end
 
