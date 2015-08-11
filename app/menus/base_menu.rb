461612181023e70ef170667e83478c55a4f9d648
root <root@homeserver>
2015-08-11 11:43:30 +0200

    Add missing license header

--- app/menus/base_menu.rb
+++ app/menus/base_menu.rb
@@ -1,3 +1,18 @@
+# DeployIt - Docker containers management software
+# Copyright (C) 2015 Nicolas Rodriguez (nrodriguez@jbox-web.com), JBox Web (http://www.jbox-web.com)
+#
+# This code is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License, version 3,
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License, version 3,
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
 class BaseMenu < SimpleDelegator
 
   USER_CONTROLLERS = [ 'MyController', 'PublicKeysController', 'RegistrationsController' ]
