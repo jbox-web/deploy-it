da8984e14540a22b87c45447f580a6524e5af3f5
root <root@homeserver>
2015-08-13 05:19:41 +0200

    Small refactoring in mailers

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -3,24 +3,26 @@ class RegistrationMailer < ActionMailer::Base
   default from: "no-reply@jbox-web.com"
 
   def welcome(user, password)
-    @user     = user
-    @password = password
-    @url      = "#{Settings.access_url}/users/login"
-    mail(to: @user.email,
-         subject: t('.title'),
-         template_path: 'mailers',
-         template_name: 'welcome')
+    render_template('welcome', user, password)
   end
 
 
   def password_changed(user, password)
-    @user     = user
-    @password = password
-    @url      = "#{Settings.access_url}/users/login"
-    mail(to: @user.email,
-         subject: t('.title'),
-         template_path: 'mailers',
-         template_name: 'password_changed')
+    render_template('password_changed', user, password)
   end
+
+
+  private
+
+
+    def render_template(template, user, password)
+      @user     = user
+      @password = password
+      @url      = "#{Settings.access_url}/users/login"
+      mail(to: @user.email,
+           subject: t('.title'),
+           template_path: 'mailers',
+           template_name: template)
+    end
 
 end
