f3d18fac5ba6e8fc8d0e9a14fffee4f842ee75b1
root <root@homeserver>
2015-09-29 04:21:26 +0200

    raise_on_missing_translations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
   config.assets.raise_runtime_errors = true
 
   # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
+  config.action_view.raise_on_missing_translations = true
 
   # Devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
