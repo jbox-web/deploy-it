660c9ad2b90439b1027aa24bec0d88b0f5eb0f4c
root <root@homeserver>
2015-10-27 15:02:07 +0100

    Puma should listen on localhost so we could curl on it

--- config/puma.rb
+++ config/puma.rb
@@ -8,16 +8,15 @@ environment ENV['RAILS_ENV'] || 'development'
 
 preload_app!
 
+port ENV['PORT'] || 5000
+
 if ENV['RAILS_ENV'] == 'production'
   daemonize       true
   pidfile         File.join(Dir.pwd, 'tmp', 'pids', 'puma.pid')
   state_path      File.join(Dir.pwd, 'tmp', 'sockets', 'puma.state')
-  bind            "unix://#{File.join(Dir.pwd, 'tmp', 'sockets', 'puma.sock')}"
   stdout_redirect File.join(Dir.pwd, 'log', 'puma.stdout.log'), File.join(Dir.pwd, 'log', 'puma.stderr.log')
 
   on_worker_boot do
     ActiveRecord::Base.establish_connection
   end
-else
-  port ENV['PORT'] || 3000
 end
