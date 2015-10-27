125d141560a8494c68339f5e1e32c2b07ebe38e0
root <root@homeserver>
2015-10-27 18:46:40 +0100

    Fix Puma restart after deploy

--- config/puma.rb
+++ config/puma.rb
@@ -11,10 +11,11 @@ preload_app!
 bind "tcp://127.0.0.1:#{(ENV['PORT'] || 5000)}"
 
 if ENV['RAILS_ENV'] == 'production'
-  daemonize       true
-  pidfile         File.join(Dir.pwd, 'tmp', 'pids', 'puma.pid')
-  state_path      File.join(Dir.pwd, 'tmp', 'sockets', 'puma.state')
-  stdout_redirect File.join(Dir.pwd, 'log', 'puma.stdout.log'), File.join(Dir.pwd, 'log', 'puma.stderr.log')
+  daemonize            true
+  pidfile              File.join(Dir.pwd, 'tmp', 'pids', 'puma.pid')
+  state_path           File.join(Dir.pwd, 'tmp', 'sockets', 'puma.state')
+  activate_control_app File.join(Dir.pwd, 'tmp', 'sockets', 'pumactl.sock')
+  stdout_redirect      File.join(Dir.pwd, 'log', 'puma.stdout.log'), File.join(Dir.pwd, 'log', 'puma.stderr.log')
 
   on_worker_boot do
     ActiveRecord::Base.establish_connection
