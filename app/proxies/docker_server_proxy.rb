a909b89d563085239be37e0b3962573e2d8526d2
root <root@homeserver>
2016-04-21 23:15:26 +0200

    Use info loglevel

--- app/proxies/docker_server_proxy.rb
+++ app/proxies/docker_server_proxy.rb
@@ -191,7 +191,7 @@ class DockerServerProxy
 
     def docker_command(*args)
       args = ['-H', @url].concat(args)
-      Rails.logger.debug "docker #{args.join(' ')}"
+      Rails.logger.info "docker #{args.join(' ')}"
       DeployIt::Utils::Exec.execute('docker', args)
     end
 
