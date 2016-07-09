23d901fe7a29a3a2455ddb57e1eaf043fa6e53ff
root <root@homeserver>
2016-07-09 06:59:20 +0200

    Add missing require

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -13,6 +13,8 @@
 # You should have received a copy of the GNU Affero General Public License, version 3,
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
+require 'sidekiq/exception_handler'
+
 #
 # Configure Sidekiq/Redis
 #
