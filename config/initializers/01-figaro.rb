2bca146fbcf94dbd77abc32b39b68d8f3f2fc8ae
root <root@homeserver>
2017-06-25 22:24:50 +0200

    Improve params definition in Figaro

--- config/initializers/01-figaro.rb
+++ config/initializers/01-figaro.rb
@@ -13,56 +13,65 @@
 # You should have received a copy of the GNU Affero General Public License, version 3,
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-RAILS_REQUIRED_KEYS = [
-  'DB_ADAPTER',
-  'DB_HOST',
-  'DB_PORT',
-  'DB_NAME',
-  'DB_USER',
-  'DB_PASS',
-  'RAILS_SECRET'
-]
+APPLICATION_CONFIG = {
+  rails_config: %w(
+    SECRET_KEY_BASE
+    APPLICATION_NAME
+  ),
 
-DEPLOY_IT_REQUIRED_KEYS = [
-  # Logs
-  'LOG_LEVEL',
+  db_config: %w(
+    DB_ADAPTER
+    DB_HOST
+    DB_PORT
+    DB_NAME
+    DB_USER
+    DB_PASS
+  ),
 
-  # Redis (Sidekiq / Faye)
-  'REDIS_HOST',
-  'REDIS_PORT',
-  'REDIS_DB',
+  redis_config: %w(
+    REDIS_HOST
+    REDIS_PORT
+    REDIS_DB
+  ),
 
-  # Faye server / Async events
-  'ACCESS_URL',
-  'FAYE_SECRET',
+  app_config: %w(
+    MAIL_FROM
+    LOG_LEVEL
+    ACCESS_URL
+    FAYE_SECRET
+    AUTHENTICATION_SERVER
+    AUTHENTICATION_TOKEN
+    WHITE_LISTED_IPS
+    ACCESS_DOMAIN_NAME
+    DOCKER_REGISTRY
+    APPLICATIONS_CLONES_DIR
+    APPLICATIONS_REPOS_DIR
+    APPLICATIONS_DATAS_DIR
+    SCRIPTS_PATH
+  ),
 
-  # DeployIt authentication server
-  'AUTHENTICATION_SERVER',
-  'AUTHENTICATION_TOKEN',
-  'WHITE_LISTED_IPS',
+  devise_config: %w(
+    SESSION_TIMEOUT
+  ),
 
-  # DeployIt domain name
-  'ACCESS_DOMAIN_NAME',
-  'SESSION_TIMEOUT',
+  # smtp_config: %w(
+  #   SMTP_HOST
+  #   SMTP_PORT
+  #   SMTP_DOMAIN
+  #   SMTP_USER
+  #   SMTP_PASS
+  #   MAIL_FROM
+  # ),
 
-  # Private Docker Registry
-  'DOCKER_REGISTRY',
+  monitoring_config: %w(
+    MAIL_ON_APPLICATION_ERROR
+  ),
 
-  # DeployIt directories
-  'APPLICATIONS_CLONES_DIR',
-  'APPLICATIONS_REPOS_DIR',
-  'APPLICATIONS_DATAS_DIR',
+}.freeze
 
-  # Scripts path
-  'SCRIPTS_PATH',
-
-  # Registration mailer
-  'MAIL_FROM',
-
-  # Monitoring
-  'MAIL_ON_APPLICATION_ERROR'
-]
-
-FIGARO_REQUIRED_KEYS = RAILS_REQUIRED_KEYS + DEPLOY_IT_REQUIRED_KEYS
-
-Figaro.require_keys(*FIGARO_REQUIRED_KEYS)
+begin
+  Figaro.require_keys(*APPLICATION_CONFIG.values.flatten)
+rescue Figaro::MissingKeys => e
+  puts "\n#{e.message}"
+  exit 1
+end
